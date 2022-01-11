cab8acbfcc890d30ba1abb584e02adb0d4fdede6
rAvivek5 <benjamintesla369@gmail.com>
2022-01-11 16:48:19 +0530

    My First Commit

---
+++ covid-19-tracker/src/InfoBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+
+function InfoBox({ title, cases, total, ...props }) {
+  return (
+    <Card onClick={props.onClick} className="infoBox">
+      <CardContent>
+        {/* Title i.e. Coronavirus cases */}
+        <Typography className="infoBox__title" color="textSecondary">
+          {title}
+        </Typography>
+
+        {/* +120k Number of cases */}
+        <h2 className="infoBox__cases">{cases}</h2>
+
+        {/* 1.2M Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
